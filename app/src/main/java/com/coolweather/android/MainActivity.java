af8782eaf376b2e764eea1bd6a7b09886d4aeab2
yanglidev <yangli429@outlook.com>
2017-12-11 23:35:19 +0800

    2017年12月11日23:35:06upgrade

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -3,11 +3,14 @@ package com.coolweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.coolweather.adnroid.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
